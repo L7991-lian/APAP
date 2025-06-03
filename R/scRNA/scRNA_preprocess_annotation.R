83eb23af997c71511ee2705d17195ff48dcff320
L7991-lian <1926487130@qq.com>
2025-06-03 11:36:08 +0800

    RNA preprocess add

--- R/scRNA/scRNA_preprocess_annotation.R
+++ R/scRNA/scRNA_preprocess_annotation.R
@@ -16,7 +16,7 @@ add_prefix_cellname <- function(base_path, time_points) {
   }
   return(list_of_data)
 }
-base_path <- "/SoupX/"
+base_path <- "/data4/jinlianli/APAP/Data.analysis/2022.9.1.SoupX.0.1/SoupX/"
 time_points <- c("0h", "6h", "12h", "24h", "48h", "96h")
 all_data <- add_prefix_cellname(base_path, time_points)
 
@@ -145,19 +145,98 @@ merge.clustering <- function(SeuratObject, scale.var.features.n = 3000, harmony.
 }
 SeuratObject <- merge.clustering(SeuratObject, scale.var.features.n = 2000, harmony.var = "Group", assay = "SCT", Dims = 22,  resolution = 0.05)
 
+annotation.scRNA <- function(scRNA.data, new.cluster.ids){
+  cell.markers2 <- c("Apoc1", "Apoc3", "Fabp1", "Cyp4a14", "Gstm1", "Alb", # Hepatocyte
+                     "Clec4g", "Aqp1", "Bmp2", "Ptprb", "Dpp4", "Egfl7", "F8", "Igfbp7", # Endothelial
+                     "Cd79a" ,"Cd79b", "Ebf1", "Blnk", "Bank1", "Fcmr", "Ighd", "Ms4a1", "Pax5", "Igkc", # B
+                     "Cd3d", "Trac", "Il7r", "Txk", "Ms4a4b", "Nkg7", "Gzma", "Klrb1c", "Eomes", #T/NK
+                     "Lyz2", "Fcer1g", "Clec4f", "Cd5l", "Ctss","Vsig4", # Macrophage
+                     "Ifi30", "Cstb", "Cyba", "Cybb", "Aif1", "Cox6a2", # Dendritic
+                     "Irf8", "Runx2", "Siglech", "Klk1", "Rnase6", "Ccr9",  # Dendritic
+                     "Il1r2", "Il1b", "S100a8", "S100a9", "Lcn2", "Wfdc21",  # Neutrophil
+                     "Hbb−bs", "Hba−a1", "Alas2", "Bpgm", # Erythrocyte
+                     "Dcn", "Col3a1", "Col1a2", "Col14a1", "Sparc", # HSC
+                     "Sox9", "Spp1", "Clu", "Tm4sf4", "Epcam", "Anxa4", "Pkhd1", "Onecut2", "Hnf1b", "Tstd1", "Krt19", "Pdzk1ip1" #Cholangiocyte
+  )
+  p0 <- DotPlot(scRNA.data, features = unique(cell.markers2)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+  ggsave("Clusters.markers.Dotplot2.pdf", p0, width = 24, height = 9)
+  # annotation
+  names(new.cluster.ids) <- levels(scRNA.data$seurat_clusters)
+  scRNA.data <- RenameIdents(scRNA.data, new.cluster.ids)
+  scRNA.data$CellType <- scRNA.data@active.ident
+  # UMAP
+  p1 <- DimPlot(scRNA.data, label = T, reduction = "umap")|DimPlot(scRNA.data, label = F, reduction = "umap", group.by = "Group")
+  p3 <- DimPlot(scRNA.data, label = T, reduction = "tsne")|DimPlot(scRNA.data, label = F, reduction = "tsne", group.by = "Group")
+  ggsave("Celltype.Label.pdf", p1, width = 14, height = 8)
+  ggsave("Celltype.tSNE.pdf", p3, width = 14, height = 8)
+  return(scRNA.data)
+}
+## rename idents
+new.cluster.ids <- c("Hepatocyte", 
+                     "Endothelial",
+                     "Macrophage/Dendritic", 
+                     "T/NK",
+                     "B",
+                     "Neutrophil",
+                     "Hepatocyte",
+                     "Cholangiocyte",
+                     "Macrophage/Dendritic",
+                     "Erythrocyte",
+                     "Hepatocyte", 
+                     "HSC"
+)
+SeuratObject <- annotation.scRNA(SeuratObject, new.cluster.ids)
+SeuratObject.delect.Erythrocyte <- subset(SeuratObject, celltype != "Erythrocyte")
+SeuratObject.delect.Erythrocyte$celltype <- droplevels(SeuratObject.delect.Erythrocyte$celltype)
+SeuratObject.delect.Erythrocyte@active.ident <- droplevels(SeuratObject.delect.Erythrocyte@active.ident)
+
+Dims.Clustering <- function(SeuratObject, Dims = seq(10, 40, 5), resolution.set = 0.1){
+  library(Seurat)
+  library(ggplot2)
+  library(tidyverse)
+  # clustering
+  namefc <- c()
+  for (i in Dims) {
+    namef <- paste0("Dim", i)
+    dir.create(namef)
+    namefc <- c(namefc, namef)
+  }
+  pathwd <- getwd()
+  for (m in 1:length(Dims)) {
+    setwd(pathwd)
+    path <- paste(pathwd, namefc[m], sep = "/")
+    setwd(path)
+    SeuratObject <- SeuratObject %>%
+      RunUMAP(reduction = "harmony", dims = 1:Dims[m]) %>%
+      RunTSNE(reduction = "harmony", dims = 1:Dims[m]) %>%
+      FindNeighbors(reduction = "harmony", dims = 1:Dims[m]) %>%
+      FindClusters(reduction.type = "harmony", resolution = resolution.set) %>% identity()
+    titlegg1 <- paste0("Dims", Dims[m], ".Resolution", resolution.set)
+    titlegg2 <- paste0("Dims", Dims[m], ".Resolution", resolution.set)
+    p11 <- DimPlot(SeuratObject, reduction = "umap", label = T) + ggtitle(titlegg1)|DimPlot(SeuratObject, reduction = "umap", group.by = "Group", label = F)
+    p12 <- DimPlot(SeuratObject, reduction = "tsne", label = T) + ggtitle(titlegg2)|DimPlot(SeuratObject, reduction = "tsne", group.by = "Group", label = F)
+    ggsavename1 <- paste0("Try.UMAP.", "Dims", Dims[m], "Resolution", resolution.set, ".pdf")
+    ggsavename2 <- paste0("Try.tSNE.", "Dims", Dims[m], "Resolution", resolution.set, ".pdf")
+    ggsave(ggsavename1, plot = p11, width = 14, height = 8)
+    ggsave(ggsavename2, plot = p12, width = 14, height = 8)
+  }
+  return(SeuratObject)
+}
+SeuratObject <- Dims.Clustering(SeuratObject.delect.Erythrocyte, Dims = seq(28), resolution.set = 0.05)
+
 # check celltype markers expression
 cell.markers <- c("Apoc1", "Apoc3", "Fabp1", "Cyp4a14", "Gstm1", "Alb", # Hepatocyte
-                   "Clec4g", "Aqp1", "Bmp2", "Ptprb", "Dpp4", "Egfl7", "F8", # Endothelial
-                   "Cd79a" ,"Cd79b", "Ebf1", "Blnk", "Bank1", "Fcmr", "Ighd", "Ms4a1", "Pax5", "Igkc", # B
-                   "Cd3d", "Cd3g", #T
+                  "Clec4g", "Aqp1", "Bmp2", "Ptprb", "Dpp4", "Egfl7", "F8", # Endothelial
+                  "Cd79a" ,"Cd79b", "Ebf1", "Blnk", "Bank1", "Fcmr", "Ighd", "Ms4a1", "Pax5", "Igkc", # B
+                  "Cd3d", "Cd3g", #T
                   "Klre1", "Klrb1c", "Eomes","Gzma",  #NK
-                   "Lyz2", "Fcer1g", "Clec4f", "Cd5l", "Ctss","Vsig4", # Macrophage
-                   "Ifi30", "Cstb", "Cyba", "Cybb", "Aif1", "Cox6a2", # Dendritic
-                   "Irf8", "Runx2", "Siglech", "Klk1", "Rnase6", "Ccr9",  # Dendritic
-                   "Il1r2", "Il1b", "S100a8", "S100a9", "Lcn2", "Wfdc21",  # Neutrophil
-                   "Hbb−bs", "Hba−a1", "Alas2", "Bpgm", # Erythrocyte
-                   "Dcn", "Col3a1", "Col1a2", "Col14a1", "Sparc", # HSC
-                   "Sox9", "Spp1", "Clu", "Hnf1b", "Tstd1", "Krt19" #Cholangiocyte
+                  "Lyz2", "Fcer1g", "Clec4f", "Cd5l", "Ctss","Vsig4", # Macrophage
+                  "Ifi30", "Cstb", "Cyba", "Cybb", "Aif1", "Cox6a2", # Dendritic
+                  "Irf8", "Runx2", "Siglech", "Klk1", "Rnase6", "Ccr9",  # Dendritic
+                  "Il1r2", "Il1b", "S100a8", "S100a9", "Lcn2", "Wfdc21",  # Neutrophil
+                  "Hbb−bs", "Hba−a1", "Alas2", "Bpgm", # Erythrocyte
+                  "Dcn", "Col3a1", "Col1a2", "Col14a1", "Sparc", # HSC
+                  "Sox9", "Spp1", "Clu", "Hnf1b", "Tstd1", "Krt19" #Cholangiocyte
 )
 p0 <- DotPlot(SeuratObject, features = unique(cell.markers)) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 p0
@@ -189,4 +268,31 @@ celltype.markers <- c("Alb", # Hepatocyte
                       "Dcn" # HSC
 )
 DotPlot(SeuratObject, group.by = "celltype", features = celltype.markers) 
-saveRDS(SeuratObject, "APAP_scRNA_sce.Rds")
+celltype.markers <- c("Alb", # Hepatocyte
+                      "Ptprb", # Endothelial
+                      "Cybb", # Macrophage
+                      "Cd3d", #T
+                      "Ebf1", # B
+                      "S100a9", # Neutrophil
+                      "Klre1", #NK
+                      "Sox9", #Cholangiocyte
+                      "Dcn" # HSC
+)
+DotPlot(SeuratObject, group.by = "celltype", features = celltype.markers) 
+pdf("scRNA_celltype_markers_dotplot.pdf")
+jjDotPlot(object = SeuratObject, 
+          col.min = 0,
+          anno = T,
+          base_size = 14,
+          textSize = 14, 
+          id = 'celltype',
+          gene = celltype.markers,
+          xtree = F, ytree = F, 
+          legend.position = "right")
+dev.off()
+colors <- c(rgb(247/255,147/255,30/255), rgb(248/255,115/255,106/255), rgb(169/255,169/255,169/255), rgb(150/255,206/255,180/255), rgb(163/255,165/255,0/255), rgb(192/255,193/255,48/255), rgb(157/255,115/255,194/255), rgb(183/255,76/255,171/255), rgb(230/255,134/255,201/255), rgb(140/255,198/255,63/255), rgb(255/255,191/255,15/255),
+            rgb(103/255,199/255,193/255), rgb(3/255,161/255,198/255), rgb(97/255,156/255,255/255), rgb(129/255,70/255,58/255), rgb(0/255,114/255,189/255), rgb(74/255,76/255,191/255))
+pdf("scrna_UMAP.pdf")
+DimPlot(SeuratObject, group.by = "celltype", cols = colors)+ theme(legend.title = element_blank(), legend.text = element_text(size = 14)) + ggtitle("")| DimPlot(All_obj, group.by = "Group", cols = colors)+ theme(legend.title = element_blank(), legend.text = element_text(size = 14)) + ggtitle("")
+dev.off()
+saveRDS(SeuratObject, "/data4/jinlianli/APAP/Data.analysis/Object/2.Merge/All.Rds")
